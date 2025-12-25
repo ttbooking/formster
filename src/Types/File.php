5a0da33ad6116fffda0026023273ef3b28076727
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2025-12-25 11:51:07 +0200

    File type: check only files for existence, not directories

--- src/Types/File.php
+++ src/Types/File.php
@@ -55,7 +55,7 @@ class File implements Castable, Comparable, JsonSerializable, Stringable
 
     public function exists(): bool
     {
-        return Storage::disk($this->disk)->exists($this->name);
+        return Storage::disk($this->disk)->fileExists($this->name);
     }
 
     public function size(): int
