8ebe8537c91c50b8f7e45ddddd65b14c06d6318e
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2026-07-22 13:54:03 +0200

    Model component: "id" renamed to "key" to eliminate confusion with HTML element's id

--- resources/views/components/form/model.blade.php
+++ resources/views/components/form/model.blade.php
@@ -6,8 +6,8 @@
     <span {{ $attributes }}>{{ prop_val($property, $object)->$titleColumn }}</span>
 @else
     <select {{ $attributes }} name="{{ $property->variableName }}" @disabled(! $property->writable)>
-        @foreach ($options as $id => $title)
-            <option value="{{ $id }}" @selected($id === old($property->variableName, $object->{$property->variableName}?->getKey()))>
+        @foreach ($options as $key => $title)
+            <option value="{{ $key }}" @selected($key === old($property->variableName, $object->{$property->variableName}?->getKey()))>
                 {{ $title }}
             </option>
         @endforeach
