c6f3f4215fbaa812496e3228866348b5303d28d9
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2026-07-22 17:23:05 +0200

    Table component: strip empty caption tags

--- resources/views/components/form/table.blade.php
+++ resources/views/components/form/table.blade.php
@@ -1,12 +1,14 @@
 <table {{ $attributes }}>
-    <caption>
-        @if (! isset($title) || $title->isEmpty())
-            <h4>{!! Str::inlineMarkdown($summary) !!}</h4>
-            <h5>{!! Str::markdown($description) !!}</h5>
-        @else
-            {{ $title }}
-        @endif
-    </caption>
+    @if (isset($title) && $title->isNotEmpty() || $summary || $description)
+        <caption>
+            @if (! isset($title) || $title->isEmpty())
+                @if ($summary)<h4>{!! Str::inlineMarkdown($summary) !!}</h4>@endif
+                @if ($description)<h5>{!! Str::markdown($description) !!}</h5>@endif
+            @else
+                {{ $title }}
+            @endif
+        </caption>
+    @endif
     <thead>
         <th>{{ __('formster::form.description') }}</th>
         <th>{{ __('formster::form.value') }}</th>
