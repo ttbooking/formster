7e3d89395bd2bf1da9013fcfcf19ef311f9d1dc1
Sergey Danilchenko <s.danilchenko@ttbooking.ru>
2026-07-23 16:22:29 +0200

    Form component: passthrough title slot contents into child table component

--- resources/views/components/form/form.blade.php
+++ resources/views/components/form/form.blade.php
@@ -1,7 +1,11 @@
 <form {{ $attributes->only(['id', 'action', 'enctype'])->merge($mergeAttrs) }} method="POST">
     @csrf
     @method('PUT')
-    <x-formster::form.table {{ $attributes->except(['id', 'enctype']) }} :$object :$showDefaults :editable="true" />
+    <x-formster::form.table {{ $attributes->except(['id', 'enctype']) }} :$object :$showDefaults :editable="true">
+        @isset ($title)
+            <x-slot:title>{{ $title }}</x-slot:title>
+        @endisset
+    </x-formster::form.table>
     @if (! isset($buttons) || $buttons->isEmpty())
         @can ($aura->updatePolicy, $object)
             <button type="submit">{{ __('formster::form.save') }}</button>
